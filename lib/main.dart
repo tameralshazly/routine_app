2d532def5b41a5db11e5c7a1c4f82bf65e23f85f
tameralshazly <tameralshazly@yahoo.com>
2023-08-12 20:37:24 +0400

    step-12

--- lib/main.dart
+++ lib/main.dart
@@ -47,9 +47,17 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List<Routine>? routines;
+  @override
+  void initState() {
+    super.initState();
+    _readRoutines();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueAccent,
       appBar: AppBar(
         title: const Text('Routine'),
         actions: [
@@ -66,6 +74,81 @@ class _MainPageState extends State<MainPage> {
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: _buildWidgets(),
+        ),
+      ),
     );
+  }
+
+  _readRoutines() async {
+    final routineCollection = widget.isar.routines;
+    final getRoutines = await routineCollection.where().findAll();
+    setState(() {
+      routines = getRoutines;
+    });
+  }
+
+  List<Widget> _buildWidgets() {
+    List<Widget> x = [];
+
+    for (var i = 0; i < routines!.length; i++) {
+      x.add(Card(
+        elevation: 4.0,
+        child: ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 2.0),
+                child: Text(
+                  routines![i].title,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: RichText(
+                  text: TextSpan(
+                    style: const TextStyle(color: Colors.black),
+                    children: [
+                      const WidgetSpan(
+                          child: Icon(
+                        Icons.schedule,
+                        size: 16,
+                      )),
+                      TextSpan(
+                        text: routines![i].startTime,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: RichText(
+                  text: TextSpan(
+                    style: const TextStyle(color: Colors.black, fontSize: 12),
+                    children: [
+                      const WidgetSpan(
+                          child: Icon(
+                        Icons.calendar_month,
+                        size: 16,
+                      )),
+                      TextSpan(
+                        text: routines![i].day,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          trailing: const Icon(Icons.keyboard_arrow_right),
+        ),
+      ));
+    }
+    return x;
   }
 }
